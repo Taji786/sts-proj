4e45cdef0fd5f5362d68c879304cc9f2696d6ef6
Taji786 <shaiksamreen99@gmail.com>
2021-05-31 06:30:16 +0530

    changing ide code

--- src/main/java/com/example/demo/AatkApplication.java
+++ src/main/java/com/example/demo/AatkApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AatkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AatkApplication.class, args);
+	SpringApplication.run(AatkApplication.class, args);
+	  int i=10;
 	}
 
 }
