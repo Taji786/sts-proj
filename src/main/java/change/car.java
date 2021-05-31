b36b20fb69e99c55074482cc1fdf4f68224b0f86
Taji786 <shaiksamreen99@gmail.com>
2021-05-31 19:59:59 +0530

    dileep proj

--- src/main/java/change/car.java
+++ src/main/java/change/car.java
@@ -1,7 +1,7 @@
 package change;
 
 public class car {
-
+     public static String engine="Honda";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       public void start()
