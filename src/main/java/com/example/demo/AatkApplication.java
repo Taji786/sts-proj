3185c2f40ac6af4eb26e2eb2a62b13d76caa3ff6
Taji786 <shaiksamreen99@gmail.com>
2021-05-31 06:42:38 +0530

    after stash

--- src/main/java/com/example/demo/AatkApplication.java
+++ src/main/java/com/example/demo/AatkApplication.java
@@ -9,6 +9,8 @@ public class AatkApplication {
 	public static void main(String[] args) {
 	SpringApplication.run(AatkApplication.class, args);
 	  int i=10;
+	  //before stash checking
+	  int j=20;
 	}
 
 }
